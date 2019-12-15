33c64565ea28478a2619fd0dda856832854b0c33
mak1k1 <martin.cehelsky.ja@gmail.com>
2019-12-15 19:57:28 +0100

    Added storing file in storage, added storage link

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,14 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use \App\Post;
 
 class PostsController extends Controller
-{
+{   
+    public function __construct(){
+        $this->middleware('auth');
+    }
+
     public function create(){
         return view('posts.create');
     }
 
     public function store(){
+        $data = request()->validate([
+            'caption' => 'required',
+            'image' => ['required', 'image'],
+            
+        ]);
+
+        dd(request('image')->store('uploads','public'));
+
+        auth()->user()->posts()->create($data);
+
         dd(request()->all());
     }
 }
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -7,34 +7,27 @@
         <div class="row">
             <h1>Add New Post</h1>
         </div>
-        <div class=" row">
-            <div class="col-8 offset-2">
-                <div class="form-group row">
-                    <label for="caption" class="col-md-4 col-form-label">Post Caption</label>
+            <div class="form-group row">
+                <label for="caption" class="col-md-4 col-form-label">Post Caption</label>
 
-                    <input id="caption" type="text" class="form-control @error('caption') is-invalid @enderror"
-                        name="caption" value="{{ old('caption') }}" required autocomplete="caption" autofocus>
+                <input id="caption" type="text" class="form-control @error('caption') is-invalid @enderror"
+                    name="caption" value="{{ old('caption') }}" required autocomplete="caption" autofocus>
 
-                    @error('caption')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                    @enderror
-
-                </div>
+                @error('caption')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                @enderror
             </div>
-        </div>
         <div class="row">
             <label for="image" class="col-md-4 col-form-label">Post Caption</label>
             <input id="image" type="file" class="form-control-file" name="image">
             @error('image')
-            <span class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong>
-            </span>
             @enderror
         </div>
         <div class="row pt-4">
-            <button class="btn btn-primary">Submit</button>
+            <button class="btn btn-primary">Add new post</button>
         </div>
     </form>
 </div>
